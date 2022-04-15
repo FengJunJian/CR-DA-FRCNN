b8d6caf22e83d8cf47ea9220df74bd07844a54ca
FengJunJian <446187960@qq.com>
2022-04-15 11:04:19 +0800

    add SW_DA_RCNN

--- maskrcnn_benchmark/modeling/detector/detectors.py
+++ maskrcnn_benchmark/modeling/detector/detectors.py
@@ -1,8 +1,8 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-from .generalized_rcnn import GeneralizedRCNN
+from .generalized_rcnn import GeneralizedRCNN,SW_DA_RCNN
 
 
-_DETECTION_META_ARCHITECTURES = {"GeneralizedRCNN": GeneralizedRCNN}
+_DETECTION_META_ARCHITECTURES = {"GeneralizedRCNN": GeneralizedRCNN,"SW_DA_RCNN":SW_DA_RCNN}
 
 
 def build_detection_model(cfg):
