c0b31b786b19e91113440487c5acdc6643127e0e
FengJunJian <446187960@qq.com>
2022-04-15 21:28:02 +0800

    delete

--- test_net_test1283semi.bat
+++
@@ -1,4 +0,0 @@
-python tools/test_net.py --config-file configs/da_ship/da_faster_rcnn_R_50_C4_SeaShips_SMD00.yaml MODEL.DOMAIN_ADAPTATION_ON False ^
-DATASETS.TEST ('test1283_cocostyle',) OUTPUT_DIR semilog00
-
-
