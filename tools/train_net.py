a8a1b674c80f2718ec94313f476da68785cf62a2
FengJunJian <446187960@qq.com>
2022-04-15 21:26:52 +0800

    update

--- tools/train_net.py
+++ tools/train_net.py
@@ -17,7 +17,7 @@ from maskrcnn_benchmark.config import cfg
 from maskrcnn_benchmark.data import make_data_loader
 from maskrcnn_benchmark.solver import make_lr_scheduler
 from maskrcnn_benchmark.solver import make_optimizer
-from maskrcnn_benchmark.engine.inference import inference
+#from maskrcnn_benchmark.engine.inference import inference
 from maskrcnn_benchmark.engine.trainer import do_train, do_da_train,do_da_sw_train
 from maskrcnn_benchmark.modeling.detector import build_detection_model
 from maskrcnn_benchmark.utils.checkpoint import DetectronCheckpointer
