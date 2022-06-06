5195f390c96107a9e0e342dcb206668791771d47
FengJunJian <446187960@qq.com>
2022-06-06 09:57:10 +0800

    updates

--- maskrcnn_benchmark/modeling/da_heads/da_heads.py
+++ maskrcnn_benchmark/modeling/da_heads/da_heads.py
@@ -297,7 +297,6 @@ class SWDomainModule(torch.nn.Module):
         ############################################################################################
         batch=img_features1.shape[0]
 
-
         SW_netD_pixelInputFeatures = self.grl_SW(img_features1)
         feat_pixel=None
         if self.lc:
